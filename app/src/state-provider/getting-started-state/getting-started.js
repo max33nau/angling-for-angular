65c989c9ac508d0cc136de3e8a6d572c0cab620b
max33nau <mtj29@nau.edu>
2016-05-23 01:20:15 -0700

    getting started completed

--- app/src/state-provider/getting-started-state/getting-started.js
+++ app/src/state-provider/getting-started-state/getting-started.js
@@ -19,5 +19,15 @@ export default {
       </body>
     </html>`;
     $scope.javascript = `// Angular App Will GO HERE `
+    $scope.description = `/*If you did not do a git clone on my repo then go to
+    angularJS website: https://angularjs.org
+    and then click the download angular 1 button
+    and download the most stable angularJS in minified form
+    and then add it to your directory along with the
+    index.html content and a app.js file*/
+
+    /* You should now be able to run your application in
+    the browser and you should see a HELLO WORLD in paragraph
+    form. If you see this, then you are ready to get started. */`
   }]
 };
